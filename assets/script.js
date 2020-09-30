ff1d3a1e3823d947b630de17b9156a7c42c82db8
Daniel Rodriguez <drod18@hotmail.com>
2020-09-29 22:17:11 -0500

    Question selection completion and arrays created.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,48 @@
+// HTML elements getting pulled for JS function
+document.getElementById("button").onclick = function() {beginQuiz();}
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
 
-var gameTimerEl = document.getElementById("timer")
+// Questions to be used for game
+
+var questions = [
+    {
+        question : "What is the shorthand term for JavaScript?",  // Answer JS
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C",
+    },{
+        question : "How many else statements are allowed?",  // Answer Just one
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B",
+    },{
+        question : "What are the nouns of programming?",  // Answer variables
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B",
+    },{
+        question : "What copies an entire repo?", // Answer git clone
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C",
+    },{
+        question : "What does CSS Stand for?", // Answer Cascading Style Sheets
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A",
+    },
+];
 
 
-function gameTimer() {
-    var time = 60;
-    gameTimerEl.textContent = time + "seconds left";
-    var intervalId = setInterval(function(){
-      time--;
-      gameTimerEl.textContent = time + "seconds left";
-      console.log(time);
-      if(time===0){
-          clearInterval(intervalId);
-          gameTimerEl.textContent = "";
-      }
-    }, 1000)
-}
+
+
+
+
 
