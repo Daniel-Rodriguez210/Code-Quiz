c0a71de525d75ff15b9eb00f4afb70cb5f8acaed
Daniel Rodriguez <drod18@hotmail.com>
2020-11-15 21:32:58 -0600

    final app for git push all items function as liked

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,17 @@ var body = document.body;
 var questionContainerEl = document.getElementById('question-container')
 var questionEl = document.getElementById('question')
 
-
+// Variables created for usage throughout js code
 var secondsLeft = 60;
 document.getElementById("question-container").style.display = "none";
 var currentQuestionIndex = 0;
 var score = 0;
 var incorrectAnswerTimePenalty = 15;
-var answerList = document.createElement("ul")
+var answerList = document.createElement("ul");
+var timerInterval = 0;
 
+ 
+// this function is beginning of the quiz it is triggered by the user pressing the start button and calls the gameTime function
 function startGame() {
 gameTime()
 };
@@ -41,6 +44,8 @@ function render(currentQuestionIndex){
     })
 
 }
+
+// this function is looping through the questions array and checking the answers the user submits and verifying if they're correct. 
 
 function answerCheck(event) {
     var main = event.target;
@@ -72,6 +77,8 @@ function endQuiz() {
 
 }
 
+// this function keeps track of the clock for the quiz. If the user misses a question they're penalized, and it also calls the render function for continued gameplay or ends the game once the all questions answered
+
 function gameTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -86,8 +93,85 @@ function gameTime() {
     render(currentQuestionIndex);
 };
 
+// This function is the last page for the quiz where the user sees their score and can place their initials into the user input. 
+
+function endQuiz() {
+    questionEl.innerHTML = "";
+    timer.innerHTML = "";
+
+    var newHeading = document.createElement("h1");
+    newHeading.setAttribute("id", "createH1");
+    newHeading.textContent = "All Done!"
+
+    questionEl.appendChild(newHeading);
+
+    var newParagraph = document.createElement("p");
+    newParagraph.setAttribute("id", "createP");
+
+    questionEl.appendChild(newParagraph)
+
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var secondParagraph = document.createElement("p");
+        clearInterval(timerInterval);
+        newParagraph.textContent = "Your final score is: " + timeRemaining;
+
+        questionEl.appendChild(secondParagraph)
+    }
+
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+    questionEl.appendChild(createLabel);
+
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questionEl.appendChild(createInput);
+
+    var createSubmitButton = document.createElement("button");
+    createSubmitButton.setAttribute("type", "submit");
+    createSubmitButton.setAttribute("id", "Submit");
+    createSubmitButton.textContent = "Submit";
+
+    questionEl.appendChild(createSubmitButton);
+
+    // Event listener to capture initials and local storage for initials and score
+    createSubmitButton.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("Please Input Your Initials!");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var leaderboard = localStorage.getItem("leaderboard");
+            if (leaderboard === null) {
+                leaderboard = [];
+            } else {
+                leaderboard = JSON.parse(leaderboard);
+            }
+            leaderboard.push(finalScore);
+            var newScore = JSON.stringify(leaderboard);
+            localStorage.setItem("leaderboard", newScore);
+            
+            window.location.replace("index.html");
+        }
+    });
+
+}
 
 
+
+//Questions array for the quiz game
 var questions = [
     {
         name: "What is the shorthand term for JavaScript?",  // Answer JS
